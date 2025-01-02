08c812783cd24aa8211ff5a8b9bd4792ca7f0be2
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2025-01-02 19:46:41 +0530

    refactor: refactored kaplan meier analysis code

--- src/main.py
+++ src/main.py
@@ -1,76 +1,17 @@
 import argparse
-import pandas as pd
-import numpy as np
 import os
+
 from preprocess.config.config import Conf
 from preprocess.clinical import ClinicalData
 from preprocess.mutation import MutationData
 from preprocess.mirna import miRNAData
-
 from preprocess.config.patient_mappings import PatientMappings
 
-from lifelines import KaplanMeierFitter
-from lifelines.statistics import logrank_test
-from lifelines import CoxPHFitter
-
-import matplotlib.pyplot as plt
-
-from tqdm import tqdm
-
 def perform_km_analysis(mutation_data: MutationData):
-    df = mutation_data()
-
-    significant_genes = []
-    p_value_threshold = 0.1
-    min_group_size = 20
-
-    output_dir = "../results/km_test"
+    output_dir = "../results/kaplan_meier_analysis"
     os.makedirs(output_dir, exist_ok=True)
 
-    for gene in tqdm(mutation_data().columns[1:-3]):
-        
-        mutated = df[df[gene] == 1]
-        non_mutated = df[df[gene] == 0]
-
-        if len(mutated) < min_group_size or len(non_mutated) < min_group_size:
-            continue
-
-        results_logrank = logrank_test(
-            mutated["overall_survival"], non_mutated["overall_survival"],
-            event_observed_A=mutated["status"], event_observed_B=non_mutated["status"]
-        )
-
-        if results_logrank.p_value < p_value_threshold:
-            significant_genes.append({
-                "gene": gene,
-                "mutated_count": len(mutated),
-                "non_mutated_count": len(non_mutated),
-                "mutated_mean_survival (days)": round(mutated['overall_survival'].mean(), 2),
-                "non_mutated_mean_survival (days)": round(non_mutated['overall_survival'].mean(), 2),
-                "p_value": results_logrank.p_value,
-            })
-        
-            kmf = KaplanMeierFitter()
-            
-            plt.figure(figsize=(10, 6))
-            kmf.fit(mutated["overall_survival"], event_observed=mutated["status"], label="Mutated")
-            kmf.plot_survival_function()
-            
-            kmf.fit(non_mutated["overall_survival"], event_observed=non_mutated["status"], label="Non-Mutated")
-            kmf.plot_survival_function()
-            
-            plt.title(f"Kaplan-Meier Survival Curve for {gene} Mutation")
-            plt.xlabel("Time (days)")
-            plt.ylabel("Survival Probability")
-            plt.legend()
-            plt.grid(True)
-            
-            plot_filename = os.path.join(output_dir, f"plots/{gene}_km_curve_paad.png")
-            plt.savefig(plot_filename, dpi=300)
-            plt.close()  
-
-    significant_genes_df = pd.DataFrame(significant_genes)
-    significant_genes_df.to_csv("../results/km_test/significant_genes_paad.csv")
+    mutation_data.kaplan_meier_analysis(output_dir)
 
 def perform_cox_regression(mirna_data: miRNAData):
     output_dir = "../results/cox_regression"
@@ -92,17 +33,18 @@ if __name__ == "__main__":
 
     clinical_data()['pathologic_stage'].isna().sum()
 
-
     if args.km:
+
         print("Starting Kaplan-Meier analysis...")
 
-        mutation_data = MutationData(Conf.datasets['miRNA'])
+        mutation_data = MutationData(Conf.datasets['Mutation'])
         mutation_data.add_stage_data(clinical_data)
         mutation_data.clean_data()
 
         perform_km_analysis(mutation_data)
 
     elif args.cx:
+
         print("Starting Cox-Regression analysis...")
 
         mirna_data = miRNAData(Conf.datasets['miRNA'])
--- src/preprocess/mutation.py
+++ src/preprocess/mutation.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+import os
+from lifelines import KaplanMeierFitter
+from lifelines.statistics import logrank_test
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
 from preprocess.clinical import ClinicalData
 
 class MutationData:
@@ -33,6 +39,59 @@ class MutationData:
     def add_stage_data(self, clinical_data: ClinicalData) -> None:
         self.merge_data(clinical_data)
         self.select_common_patients()
+    
+    def kaplan_meier_analysis(self, output_dir: str) -> None:
+        significant_genes = []
+        p_value_threshold = 0.1
+        min_group_size = 20
+
+        for gene in tqdm(self.df.columns[1:-3]):
+            
+            mutated = self.df[self.df[gene] == 1]
+            non_mutated = self.df[self.df[gene] == 0]
+
+            if len(mutated) < min_group_size or len(non_mutated) < min_group_size:
+                continue
+
+            results_logrank = logrank_test(
+                mutated["overall_survival"], non_mutated["overall_survival"],
+                event_observed_A=mutated["status"], event_observed_B=non_mutated["status"]
+            )
+
+            if results_logrank.p_value < p_value_threshold:
+                significant_genes.append({
+                    "gene": gene,
+                    "mutated_count": len(mutated),
+                    "non_mutated_count": len(non_mutated),
+                    "mutated_mean_survival (days)": round(mutated['overall_survival'].mean(), 2),
+                    "non_mutated_mean_survival (days)": round(non_mutated['overall_survival'].mean(), 2),
+                    "p_value": results_logrank.p_value,
+                })
+            
+                kmf = KaplanMeierFitter()
+                
+                plt.figure(figsize=(10, 6))
+                kmf.fit(mutated["overall_survival"], event_observed=mutated["status"], label="Mutated")
+                kmf.plot_survival_function()
+                
+                kmf.fit(non_mutated["overall_survival"], event_observed=non_mutated["status"], label="Non-Mutated")
+                kmf.plot_survival_function()
+                
+                plt.title(f"Kaplan-Meier Survival Curve for {gene} Mutation")
+                plt.xlabel("Time (days)")
+                plt.ylabel("Survival Probability")
+                plt.legend()
+                plt.grid(True)
+
+                _output_dir = os.path.join(output_dir, "plots")
+                os.makedirs(_output_dir, exist_ok=True)
+                                
+                plot_filename = os.path.join(_output_dir, f"{gene}_km_curve_paad.png")
+                plt.savefig(plot_filename, dpi=300)
+                plt.close()  
+
+        significant_genes_df = pd.DataFrame(significant_genes)
+        significant_genes_df.to_csv(os.path.join(output_dir, "significant_genes_paad.csv"))
         
     def __call__(self) -> pd.DataFrame:
         return self.df
